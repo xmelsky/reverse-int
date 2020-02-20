bef68e343d4dc2b30fcdfb71bc90e8d018f6d0db
dima xmiel <dima.hmelios@gmail.com>
2020-02-20 14:11:50 +0100

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return parseInt(
+        n
+            .toString()
+            .split("")
+            .reverse()
+            .join("")
+    );
+};
